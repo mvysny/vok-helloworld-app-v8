776cda3e207578192db7c55499dd7905092dad1a
Martin Vysny <mavi@vaadin.com>
2017-07-09 21:00:43 +0300

    Refactoring to ArticleEditor.kt

---
+++ web/src/main/kotlin/com/example/vok/ArticleEditor.kt
@@ -0,0 +1,39 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.*
+import com.vaadin.server.UserError
+import com.vaadin.ui.*
+import com.vaadin.ui.themes.ValoTheme
+
+class ArticleEditor : VerticalLayout() {
+    private val binder = beanValidationBinder<Article>()
+    var article: Article? = null
+        set(value) {
+            field = value
+            if (value != null) binder.readBean(value)
+        }
+
+    init {
+        isMargin = false
+        textField("Title") {
+            bind(binder).bind(Article::title)
+        }
+        textArea("Text") {
+            bind(binder).bind(Article::text)
+        }
+        button("Save Article", { event ->
+            val article = article!!
+            if (binder.validate().isOk && binder.writeBeanIfValid(article)) {
+                article.save()
+                ArticleView.navigateTo(article.id!!)
+            } else {
+                event.button.componentError = UserError("There are invalid fields")
+            }
+        })
+        button("Back", { navigateToView<ArticlesView>() }) {
+            styleName = ValoTheme.BUTTON_LINK
+        }
+    }
+}
+
+fun HasComponents.articleEditor(block: ArticleEditor.()->Unit = {}) = init(ArticleEditor(), block)
--- web/src/main/kotlin/com/example/vok/CreateArticleView.kt
+++ web/src/main/kotlin/com/example/vok/CreateArticleView.kt
@@ -2,34 +2,18 @@ package com.example.vok
 
 import com.github.vok.karibudsl.*
 import com.vaadin.navigator.*
-import com.vaadin.server.UserError
 import com.vaadin.ui.VerticalLayout
 import com.vaadin.ui.themes.ValoTheme
 
 @AutoView
 class CreateArticleView: VerticalLayout(), View {
-    private val binder = beanValidationBinder<Article>()
+    private val editor: ArticleEditor
     init {
         label("New Article") {
             styleName = ValoTheme.LABEL_H1
         }
-        textField("Title") {
-            bind(binder).bind(Article::title)
-        }
-        textArea("Text") {
-            bind(binder).bind(Article::text)
-        }
-        button("Save Article", { event ->
-            val article = Article()
-            if (binder.validate().isOk && binder.writeBeanIfValid(article)) {
-                article.save()
-                ArticleView.navigateTo(article.id!!)
-            } else {
-                event.button.componentError = UserError("There are invalid fields")
-            }
-        })
-        button("Back", { navigateToView<ArticlesView>() }) {
-            styleName = ValoTheme.BUTTON_LINK
+        editor = articleEditor {
+            article = Article()
         }
     }
     override fun enter(event: ViewChangeListener.ViewChangeEvent?) {
