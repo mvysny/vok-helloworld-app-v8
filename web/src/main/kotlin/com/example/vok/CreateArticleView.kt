cb0492a0918c6de35348d0ab8feb9fd7b3db273e
Martin Vysny <mavi@vaadin.com>
2017-07-09 20:03:48 +0300

    Added CreateArticleView.kt

---
+++ web/src/main/kotlin/com/example/vok/CreateArticleView.kt
@@ -0,0 +1,27 @@
+package com.example.vok
+
+import com.github.vok.karibudsl.*
+import com.vaadin.navigator.View
+import com.vaadin.navigator.ViewChangeListener
+import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.themes.ValoTheme
+
+@AutoView
+class CreateArticleView: VerticalLayout(), View {
+    private val binder = beanValidationBinder<Article>()
+    init {
+        label("New Article") {
+            styleName = ValoTheme.LABEL_H1
+        }
+        textField("Title") {
+            bind(binder).bind(Article::title)
+        }
+        textArea("Text") {
+            bind(binder).bind(Article::text)
+        }
+        button("Save Article", {
+        })
+    }
+    override fun enter(event: ViewChangeListener.ViewChangeEvent?) {
+    }
+}
