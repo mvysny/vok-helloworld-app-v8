83c6bf065d1193f48879761832f4befb1874672a
Martin Vysny <mavi@vaadin.com>
2017-07-10 11:54:21 +0300

    ArticleView: added support for editing/listing comments

--- web/src/main/kotlin/com/example/vok/ArticleView.kt
+++ web/src/main/kotlin/com/example/vok/ArticleView.kt
@@ -1,8 +1,10 @@
 package com.example.vok
 
 import com.github.vok.framework.sql2o.get
+import com.github.vok.framework.sql2o.vaadin.getAll
 import com.github.vok.karibudsl.*
 import com.vaadin.navigator.*
+import com.vaadin.server.UserError
 import com.vaadin.ui.*
 import com.vaadin.ui.themes.ValoTheme
 
@@ -11,12 +13,26 @@ class ArticleView: FormLayout(), View {
     private lateinit var article: Article
     private val title: Label
     private val text: Label
+    private val comments: Label
+    private val commentBinder = beanValidationBinder<Comment>()
+    private lateinit var createComment: Button
     init {
         title = label {
             caption = "Title:"
         }
         text = label {
             caption = "Text:"
+        }
+        comments = label { caption = "Comments" }
+        formLayout {
+            caption = "Add a comment:"
+            textField("Commenter:") {
+                bind(commentBinder).bind(Comment::commenter)
+            }
+            textField("Body:") {
+                bind(commentBinder).bind(Comment::body)
+            }
+            createComment = button("Create", { createComment() })
         }
         button("Edit", { EditArticleView.navigateTo(article.id!!) }) {
             styleName = ValoTheme.BUTTON_LINK
@@ -31,7 +47,26 @@ class ArticleView: FormLayout(), View {
         title.value = article.title
         text.value = article.text
     }
-
+    private fun createComment() {
+        val comment = Comment()
+        if (!commentBinder.validate().isOk || !commentBinder.writeBeanIfValid(comment)) {
+            createComment.componentError = UserError("There are invalid fields")
+        } else {
+            createComment.componentError = null
+            comment.article_id = article.id
+            comment.save()
+            refreshComments()
+            commentBinder.readBean(Comment())  // this clears the comment fields
+        }
+    }
+    private fun refreshComments() {
+        comments.html(
+            // force-update the comments list.
+            article.comments.getAll().joinToString("") { comment ->
+                "<p><strong>Commenter:</strong>${comment.commenter}</p><p><strong>Comment:</strong>${comment.body}</p>"
+            }
+        )
+    }
     companion object {
         fun navigateTo(articleId: Long) = navigateToView<ArticleView>(articleId.toString())
     }
