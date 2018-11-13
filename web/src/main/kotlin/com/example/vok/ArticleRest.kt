04fe3705689a4cf30f0086463301f86d2bc2fe7f
Martin Vysny <martin@vysny.me>
2018-11-13 09:02:52 +0200

    Fixed ArticleRest

--- web/src/main/kotlin/com/example/vok/ArticleRest.kt
+++ web/src/main/kotlin/com/example/vok/ArticleRest.kt
@@ -7,12 +7,12 @@ import io.javalin.NotFoundResponse
 
 fun Javalin.articleRest() {
     get("/rest/articles/:id") { ctx ->
-        val id = ctx.queryParam("id")!!.toLong()
+        val id = ctx.pathParam("id").toLong()
         ctx.json(Article.findById(id) ?: throw NotFoundResponse("No article with id $id"))
     }
     get("/rest/articles") { ctx -> ctx.json(Article.findAll()) }
     get("/rest/articles/:id/comments") { ctx ->
-        val id = ctx.queryParam("id")!!.toLong()
+        val id = ctx.pathParam("id").toLong()
         val article = Article.findById(id) ?: throw NotFoundResponse("No article with id $id")
         ctx.json(article.comments.getAll())
     }
