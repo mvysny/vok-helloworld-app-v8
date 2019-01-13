0de5957529fd718ce78c7eb9c929380d6a4f156d
Martin Vysny <martin@vysny.me>
2019-01-13 13:57:29 +0200

    MyWelcomeView ported to Composite

--- web/src/main/kotlin/com/example/vok/MyWelcomeView.kt
+++ web/src/main/kotlin/com/example/vok/MyWelcomeView.kt
@@ -2,20 +2,18 @@ package com.example.vok
 
 import com.github.mvysny.karibudsl.v8.*
 import com.vaadin.navigator.View
-import com.vaadin.ui.VerticalLayout
+import com.vaadin.ui.Composite
 import com.vaadin.ui.themes.ValoTheme
 
 @AutoView("")
-class MyWelcomeView: VerticalLayout(), View {
-    init {
-        verticalLayout {
-            label("Hello, Vaadin-on-Kotlin!") {
-                styleName = ValoTheme.LABEL_H1
-            }
-            button("My Blog") {
-                styleName = ValoTheme.BUTTON_LINK
-                onLeftClick { navigateToView<ArticlesView>() }
-            }
+class MyWelcomeView : Composite(), View {
+    private val root = verticalLayout {
+        label("Hello, Vaadin-on-Kotlin!") {
+            styleName = ValoTheme.LABEL_H1
+        }
+        button("My Blog") {
+            styleName = ValoTheme.BUTTON_LINK
+            onLeftClick { navigateToView<ArticlesView>() }
         }
     }
 }
